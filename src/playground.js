b67987ca1e23a0b52f02a7c6c18d9191c6f5b54a
Tanima Ranjan <tanimaranjan@gmail.com>
2018-11-18 18:38:41 -0500

    test

--- src/playground.js
+++ src/playground.js
@@ -1,53 +1,86 @@
-const user = {
-    name:'Tanima', 
-    cities:['Jersey City','Hoboken', 'New York'],
-    placesLived() {
-        return this.cities.map((city) => `${this.name} have lived in ${city + '!'}` )
+// const user = {
+//     name:'Tanima', 
+//     cities:['Jersey City','Hoboken', 'New York'],
+//     placesLived() {
+//         return this.cities.map((city) => `${this.name} have lived in ${city + '!'}` )
         
-    }
-}
+//     }
+// }
 
-console.log(user.placesLived());
+// console.log(user.placesLived());
 
-const multipliers = {
-    numbers:[1,2,3,4,5],
-    multiplier:2,
-    multiply() {
-       return this.numbers.map((number) => number * this.multiplier)
-    }
+// const multipliers = {
+//     numbers:[1,2,3,4,5],
+//     multiplier:2,
+//     multiply() {
+//        return this.numbers.map((number) => number * this.multiplier)
+//     }
 
-}
+// }
 
-console.log(multipliers.multiply())
-
-
-
-let count = 0
-const addOne = () => {
-    count++;
-    renderCounterApp();
-}
-
-const minusOne = () => {
-    count--;
-    renderCounterApp();
-}
-const reset = () => {
-    count=0;
-    renderCounterApp()
-}
+// console.log(multipliers.multiply())
 
 
-const renderCounterApp = () => {
 
-        const templateNum = (<div><h2>Count: {count}</h2>
-            <button onClick={addOne}>+1</button>
-            <button onClick={minusOne}>-1</button>
-            <button onClick={reset}>Reset</button>
-            </div>
-        );
+// let count = 0
+// const addOne = () => {
+//     count++;
+//     renderCounterApp();
+// }
+
+// const minusOne = () => {
+//     count--;
+//     renderCounterApp();
+// }
+// const reset = () => {
+//     count=0;
+//     renderCounterApp()
+// }
+
+
+// const renderCounterApp = () => {
+
+//         const templateNum = (<div><h2>Count: {count}</h2>
+//             <button onClick={addOne}>+1</button>
+//             <button onClick={minusOne}>-1</button>
+//             <button onClick={reset}>Reset</button>
+//             </div>
+//         );
         
-ReactDOM.render(templateNum, document.querySelector('#temp'))
+// ReactDOM.render(templateNum, document.querySelector('#temp'))
+// }
+
+// renderCounterApp();
+
+console.log("In Todo JS")
+
+const React = require('react')
+const ReactDOM = require('react-dom')
+
+let visibility = false
+
+const changeButtonTitle = () => {
+    visibility = !visibility
+
+    // console.log(buttonTitle)
+    renderVisibility()
 }
 
-renderCounterApp();
+const renderVisibility = () => {
+    
+    const templateNum = (
+        <div>
+        <h1>Visibility Toggle</h1>
+        <button onClick={changeButtonTitle}>
+        
+        {visibility ? 'Hide Details' : 'Show Details'}
+        
+        </button>
+        {visibility && <p>Show some data here </p>}
+        </div>        
+        )
+    ReactDOM.render(templateNum, document.querySelector('#temp'))
+}
+
+renderVisibility()
+
