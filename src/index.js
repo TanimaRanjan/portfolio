9acfb61a3577c28293324e19bbeb97943086f0ed
Tanima Ranjan <tanimaranjan@gmail.com>
2018-11-18 18:37:29 -0500

    Index js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,10 @@
 // Application starts here. It is the initialization file 
 console.log("Starting To Do App")
 
+ const indecisionJS = require('./indecision.js')
 
-const indecisionJS = require('./indecision.js')
+//const indecisionJS = require('./playground.js')
+//const es6 = require('./e6-class.js')
+//const counterJs = require('./visibility-toggle.js')
+
+
