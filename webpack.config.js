c588d77f05821b731c99a2c859af82898828a5a0
Tanima Ranjan <tanimaranjan@gmail.com>
2018-11-21 22:05:12 -0500

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -25,8 +25,8 @@ module.exports = {
                 }
             }, 
             {
-                test: /\.css$/, 
-                use: ["style-loader", "css-loader"]
+                test: /\.s?css$/, 
+                use: ["style-loader", "css-loader", "sass-loader"]
             }
     ]
     },
