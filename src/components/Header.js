700f40f3dfe0b88e2dee6d1a140951c024e73022
Tanima Ranjan <tanimaranjan@gmail.com>
2019-06-24 08:11:53 -0400

    Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,11 +3,24 @@ import {BrowserRouter, Route, Switch, Link, NavLink} from 'react-router-dom'
 
 const Header = () => (
     <header>
-        <h1>PORTFOLIO</h1>
-        <NavLink to="/" activeClassName="is-active" exact={true}>Home</NavLink>
-        <NavLink to="/portfolio" activeClassName="is-active"  exact={true}>Portfolio</NavLink>
-        <NavLink to="/contact" activeClassName="is-active">Contact</NavLink>
+    <div className="boxIntro">
+        <div className="parent">
+            <div className="child">
+            <h1>
+                <div className="bold">Tanima Ranjan</div>
+                <div className="faded">Director Of</div>
+                <div className="faded">Product Design</div>
+            </h1>
+            </div>
+            </div>
+        </div>
     </header>
 )
 
 export default Header;
+
+/*
+<NavLink to="/" activeClassName="is-active" exact={true}>Home</NavLink>
+<NavLink to="/portfolio" activeClassName="is-active"  exact={true}>Portfolio</NavLink>
+<NavLink to="/contact" activeClassName="is-active">Contact</NavLink>
+*/
